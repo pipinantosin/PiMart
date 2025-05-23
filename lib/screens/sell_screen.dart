6717252577235fe945d31541a781b767cde47634
Pipin Antosin <77954264+pipinantosin@users.noreply.github.com>
2025-05-23 08:21:18 +0700

    Update sell_screen.dart

--- lib/screens/sell_screen.dart
+++ lib/screens/sell_screen.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SellScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Jual")),
+      body: Center(
+        child: Text("Unggah produk atau jasa yang ingin kamu jual.", style: TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+}
 
