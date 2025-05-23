1be5cf70d509adeeca1757dbab5f284b0bdd502c
Pipin Antosin <77954264+pipinantosin@users.noreply.github.com>
2025-05-23 08:16:30 +0700

    Update account_screen.dart

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AccountScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Akun")),
+      body: Center(
+        child: Text("Kelola akun dan pengaturanmu di sini.", style: TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+}
 
