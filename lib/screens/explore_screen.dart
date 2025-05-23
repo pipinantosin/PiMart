e54ae32877c9dead80a3e56f3565d14162522c72
Pipin Antosin <77954264+pipinantosin@users.noreply.github.com>
2025-05-23 08:17:33 +0700

    Update explore_screen.dart

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ExploreScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Jelajahi")),
+      body: Center(
+        child: Text("Temukan produk dan layanan menarik.", style: TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+}
 
