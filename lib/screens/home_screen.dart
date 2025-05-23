1eeadbc6341d8a0abd56cf546a7d671d99508433
Pipin Antosin <77954264+pipinantosin@users.noreply.github.com>
2025-05-23 08:18:42 +0700

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Beranda")),
+      body: Center(
+        child: Text("Selamat datang di MartPi!", style: TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+}
 
