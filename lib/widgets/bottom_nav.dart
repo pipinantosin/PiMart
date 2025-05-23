d3cc7ab637c47c1d16a547980424dd6391c9fbd6
Pipin Antosin <77954264+pipinantosin@users.noreply.github.com>
2025-05-23 08:09:55 +0700

    Update bottom_nav.dart

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -1,1 +1,50 @@
+import 'package:flutter/material.dart';
+import '../screens/home_screen.dart';
+import '../screens/explore_screen.dart';
+import '../screens/sell_screen.dart';
+import '../screens/messages_screen.dart';
+import '../screens/account_screen.dart';
+
+class BottomNav extends StatefulWidget {
+  @override
+  _BottomNavState createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    ExploreScreen(),
+    SellScreen(),
+    MessagesScreen(),
+    AccountScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Beranda'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Jelajahi'),
+          BottomNavigationBarItem(icon: Icon(Icons.add_box), label: 'Jual'),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Pesan'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Akun'),
+        ],
+      ),
+    );
+  }
+}
 
