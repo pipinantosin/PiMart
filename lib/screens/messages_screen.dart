6aa1cbfdaf5a439f44ebe195f54eec3742750fc8
Pipin Antosin <77954264+pipinantosin@users.noreply.github.com>
2025-05-23 08:20:00 +0700

    Update messages_screen.dart

--- lib/screens/messages_screen.dart
+++ lib/screens/messages_screen.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MessagesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Pesan")),
+      body: Center(
+        child: Text("Percakapan dengan pembeli dan penjual.", style: TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+}
 
